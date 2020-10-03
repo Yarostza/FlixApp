d2fed7349e2f97fc9f8f52ac9c932519fd6eb7ac
Zachary Yarost <yarostza@msu.edu>
2020-10-03 15:54:47 -0400

    set up collection view for superhero tab

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Zachary Yarost on 10/3/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
